1ac386ef11b723f3a8c6efe98befe27ede50902e
david <davleop1999@gmail.com>
2022-03-01 21:18:07 -0600

    add some server side code...

---
+++ src/server/message.rs
@@ -0,0 +1,86 @@
+use std::{fmt::Display};
+
+use serde::{Serialize, Deserialize};
+use serde_repr::{Serialize_repr, Deserialize_repr};
+
+#[derive(Debug, Clone, Serialize_repr, Deserialize_repr, PartialEq)]
+#[repr(u8)]
+pub enum MessageType {
+    Handshake = 0,
+    Message = 1,
+    End =2,
+}
+
+pub struct ChatMessage {
+    pub r#type: MessageType,
+    pub msg: Option<String>,
+}
+
+pub enum Message {
+    Message(ChatMessage),
+    Close(),
+}
+
+impl Message {
+    pub fn new(t: MessageType) -> Message {
+        return Message::Message(GameMessage {
+            r#type: t,
+            msg: None
+        });
+    }
+
+    pub fn with_message(t: MessageType, msg: String) -> Message {
+        return Message::Message(GameMessage {
+            r#type: t,
+            msg: Some(msg),
+        });
+    }
+
+    pub fn close() -> Message {
+        return Message::Close()
+    }
+}
+
+impl TryInto<Message> for String {
+    type Error = serde_json::Error;
+
+    fn try_into(self) -> Result<Message, Self::Error> {
+        let msg = serde_json::from_str::<ChatMessage>(&self)?;
+        return Ok(Message(msg));
+    }
+}
+
+impl TryInto<String> for Message {
+    type Error = serde_json::Error;
+
+    fn try_into(self) -> Result<String, Self::Error> {
+        return match self {
+            Message::Message(msg) => serde_json::to_string(&msg),
+            _ => Ok("{}".to_string()),
+        }
+    }
+}
+
+impl TryInto<tokio_tungstenite::tungstenite::Message> for Message {
+    type Error = serde_json::Error;
+
+    fn try_into(self) -> Result<tokio_tungstenite::tungstenite::Message, Self::Error> {
+        let msg: String = self.try_into()?;
+        return Ok(tokio_tungstenite::tungstenite::Message::Text(msg));
+    }
+}
+
+impl Display for Message {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        return match self {
+            Message::Message(c) => {
+                match &c.msg {
+                    Some(m) => write!(f, "Message: {:?} {}", c.r#type, m),
+                    None => write!(f, "Message: {:?}", c.r#type)
+                }
+            }
+            Message::Close() => write!(f, "Socket Closed"),
+        };
+    }
+}
+
---
+++ src/server/socket.rs
@@ -0,0 +1,101 @@
+use std::{fmt::Display, collections::HashMap, sync::{Arc}};
+
+use futures::{stream::{SplitStream, SplitSink}, StreamExt, SinkExt};
+
+use log::error;
+use tokio::{sync::{mpsc::{Sender}, Mutex}, net::TcpStream};
+use tokio_tungstenite::{WebSocketStream, tungstenite};
+
+use crate::error::BoomerError;
+
+use super::message::{Message};
+use async_trait::async_trait;
+
+#[async_trait]
+pub trait Listenable {
+    async fn listen(&mut self, tx: Tx) -> u16;
+    async fn off(&mut self, id: u16);
+}
+
+type Tx = Sender<Message>;
+type Listener = Arc<Mutex<HashMap<u16, Tx>>>;
+type WSStream = WebSocketStream<TcpStream>;
+type MessageStream = SplitStream<WSStream>;
+
+pub struct Socket {
+    current_id: u16,
+    listeners: Listener,
+    outgoing: SplitSink<WebSocketStream<TcpStream>, tungstenite::Message>
+}
+
+pub async fn handle_messages(mut incoming: MessageStream, listeners: Listener) {
+    while let Some(Ok(msg)) = incoming.next().await {
+        if msg.is_text() {
+            let text = match msg.into_text() {
+                Err(_) => return,
+                Ok(txt) => txt,
+            };
+
+            let msg: Result<Message, serde_json::Error> = text.try_into();
+
+            if let Ok(msg) = msg {
+                let listener = listeners.lock().await;
+                for (_k, tx) in listener.iter() {
+                    tx.send(msg.clone()).await;
+                }
+            } else {
+                error!("unable to deserialize message from socket")
+            }
+        }
+    }
+}
+
+impl Socket {
+    pub async fn new(ws_stream: WSStream) -> Socket {
+        let (outgoing, incoming) = ws_stream.split();
+
+        // from network to me
+        let listeners = Arc::new(Mutex::new(HashMap::new()));
+
+        tokio::spawn(handle_messages(incoming, listeners.clone()));
+
+        return Socket {
+            outgoing,
+            listeners,
+            current_id: 0,
+        };
+    }
+
+    pub async fn push(&mut self, msg: Message) -> Result<(), BoomerError> {
+        let msg: tungstenite::Message = tungstenite::Message::Text(msg.try_into()?);
+        self.outgoing.send(msg).await;
+        return Ok(());
+    }
+
+    pub async fn close(&mut self) -> Result<(), BoomerError> {
+        self.outgoing.close().await?;
+        return Ok(());
+    }
+}
+
+#[async_trait]
+impl Listenable for Socket {
+    async fn listen(& mut self, tx: Tx) -> u16 {
+        let id = self.current_id;
+        self.current_id += 1;
+        self.listeners.lock().await.insert(id, tx);
+
+        return id;
+    }
+
+    async fn off(&mut self, id: u16) {
+        self.listeners.lock().await.remove_entry(&id);
+    }
+}
+
+impl Display for Socket {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Socket here!")
+    }
+}
+
